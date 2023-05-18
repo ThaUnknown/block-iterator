e9b24587c2ca9ac3ebbee1837bde49300ca1a8f8
Trim21 <trim21.me@gmail.com>
2023-05-18 12:18:55 +0800

    add lib ref

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+/// <reference lib="es2018.asynciterable" />
+
 type Stream<T> = Iterable<T> | AsyncIterable<T>
 
 declare function chunk<T extends Uint8Array | string>(stream: Stream<T>, size?: number, option?: {
